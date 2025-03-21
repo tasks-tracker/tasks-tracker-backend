8fecc9f3258df6d85c8867224badc24e518cf685
OsirisAnubis <olegshulgakalendar@gmail.com>
2025-03-21 21:52:37 +0300

    chore(init): delete default service & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigAdapterModule } from './adapters';
 import { CqrsAdapterModule } from './adapters';
 
@@ -9,7 +7,7 @@ import { CqrsAdapterModule } from './adapters';
     ConfigAdapterModule,
     CqrsAdapterModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
