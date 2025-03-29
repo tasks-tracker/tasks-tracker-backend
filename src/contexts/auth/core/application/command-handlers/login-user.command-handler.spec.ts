49f886daf309fbf356d8cadc564460bb59b00083
OsirisAnubis <olegshulgakalendar@gmail.com>
2025-03-29 03:25:40 +0300

    style(lint): lint

--- src/contexts/auth/core/application/command-handlers/login-user.command-handler.spec.ts
+++ src/contexts/auth/core/application/command-handlers/login-user.command-handler.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CqrsModule } from '@nestjs/cqrs';
-import { LoginUserCommandHandler } from './login-user.command-handler';
-import { LoginUserCommand } from '../commands';
-import { UserRepository, SessionRepository, CryptoPort, User, Session } from '../../domain';
-import { LoginVO, PasswordVO, PasswordHashVO, SessionIdVO, UserIdVO } from '../../domain';
-import { UserWithLoginNotExistDomainError, InvalidPasswordDomainError } from '../../domain';
 import { randomUUID } from 'node:crypto';
 import { ok } from 'neverthrow';
+
+import { LoginUserCommandHandler } from './login-user.command-handler';
+import { LoginUserCommand } from '../commands';
+
+import { UserRepository } from '../../domain';
+import { SessionRepository } from '../../domain';
+import { CryptoPort } from '../../domain';
+import { User } from '../../domain';
+import { LoginVO } from '../../domain';
+import { PasswordVO } from '../../domain';
+import { PasswordHashVO } from '../../domain';
+import { UserIdVO } from '../../domain';
+import { SessionIdVO } from '../../domain';
+import { UserWithLoginNotExistDomainError } from '../../domain';
+import { InvalidPasswordDomainError } from '../../domain';
 
 describe('LoginUserCommandHandler', () => {
   let handler: LoginUserCommandHandler;
   let cryptoPort: CryptoPort;
   let userRepository: UserRepository;
-  let sessionRepository: SessionRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -45,7 +54,6 @@ describe('LoginUserCommandHandler', () => {
     handler = module.get<LoginUserCommandHandler>(LoginUserCommandHandler);
     cryptoPort = module.get<CryptoPort>(CryptoPort);
     userRepository = module.get<UserRepository>(UserRepository);
-    sessionRepository = module.get<SessionRepository>(SessionRepository);
   });
 
   afterEach(() => {
@@ -64,23 +72,38 @@ describe('LoginUserCommandHandler', () => {
     );
     jest.spyOn(userRepository, 'getUserByLogin').mockResolvedValue(user);
 
-    const command = new LoginUserCommand(new LoginVO('valid-user'), new PasswordVO('P@ssw0rd'));
+    const command = new LoginUserCommand(
+      new LoginVO('valid-user'),
+      new PasswordVO('P@ssw0rd'),
+    );
     const result = await handler.execute(command);
 
     expect(result.isOk()).toBe(true);
     expect(result._unsafeUnwrap()).toBe('session-token');
-    expect(userRepository.getUserByLogin).toHaveBeenCalledWith(new LoginVO('valid-user'));
-    expect(cryptoPort.comparePassword).toHaveBeenCalledWith(new PasswordVO('P@ssw0rd'), user.passwordHash);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(userRepository.getUserByLogin).toHaveBeenCalledWith(
+      new LoginVO('valid-user'),
+    );
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(cryptoPort.comparePassword).toHaveBeenCalledWith(
+      new PasswordVO('P@ssw0rd'),
+      user.passwordHash,
+    );
   });
 
   it('should return error if user does not exist', async () => {
     jest.spyOn(userRepository, 'getUserByLogin').mockResolvedValue(null);
 
-    const command = new LoginUserCommand(new LoginVO('invalid-user'), new PasswordVO('P@ssw0rd'));
+    const command = new LoginUserCommand(
+      new LoginVO('invalid-user'),
+      new PasswordVO('P@ssw0rd'),
+    );
     const result = await handler.execute(command);
 
     expect(result.isErr()).toBe(true);
-    expect(result._unsafeUnwrapErr()).toBeInstanceOf(UserWithLoginNotExistDomainError);
+    expect(result._unsafeUnwrapErr()).toBeInstanceOf(
+      UserWithLoginNotExistDomainError,
+    );
   });
 
   it('should return error if password is invalid', async () => {
@@ -96,10 +119,15 @@ describe('LoginUserCommandHandler', () => {
     jest.spyOn(userRepository, 'getUserByLogin').mockResolvedValue(user);
     jest.spyOn(cryptoPort, 'comparePassword').mockResolvedValue(false);
 
-    const command = new LoginUserCommand(new LoginVO('valid-user'), new PasswordVO('wrongP@ssw0rd'));
+    const command = new LoginUserCommand(
+      new LoginVO('valid-user'),
+      new PasswordVO('wrongP@ssw0rd'),
+    );
     const result = await handler.execute(command);
 
     expect(result.isErr()).toBe(true);
-    expect(result._unsafeUnwrapErr()).toBeInstanceOf(InvalidPasswordDomainError);
+    expect(result._unsafeUnwrapErr()).toBeInstanceOf(
+      InvalidPasswordDomainError,
+    );
   });
 });
