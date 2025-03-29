b9eea52ff413e0282a05ca07d9fddec4e96ccdf6
OsirisAnubis <olegshulgakalendar@gmail.com>
2025-03-29 04:38:43 +0300

    style(auth): more good imports & command defenitions

--- src/contexts/auth/core/application/commands/register-user-by-login.command.ts
+++ src/contexts/auth/core/application/commands/register-user-by-login.command.ts
@@ -1,11 +1,10 @@
 import type { Result } from 'neverthrow';
-import { LoginVO, UserLoginAlreadyUsedDomainError } from '../../domain';
-import { PasswordVO } from '../../domain';
+import type { DomainError } from '@libs/domain-error';
+import type { LoginVO } from '../../domain';
+import type { PasswordVO } from '../../domain';
 import { Command } from '@nestjs/cqrs';
 
-export class RegisterUserByLoginCommand extends Command<
-  Result<null, UserLoginAlreadyUsedDomainError>
-> {
+export class RegisterUserByLoginCommand extends Command<Result<null, DomainError>> {
   constructor(
     public readonly login: LoginVO,
     public readonly password: PasswordVO,
