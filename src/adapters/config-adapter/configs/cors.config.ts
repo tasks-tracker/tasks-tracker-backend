3314ec4bc6792f5b29d5b7b110cb429ec5d9d131
Vitaly Safonov <vitaliysafonov@livedune.com>
2025-09-08 09:06:29 +0300

    fix: обновить команды миграции и конфигурации для разработки

--- src/adapters/config-adapter/configs/cors.config.ts
+++ src/adapters/config-adapter/configs/cors.config.ts
@@ -9,7 +9,8 @@ import { IsNumber } from 'class-validator';
 import { Yaml } from '@libs/yaml';
 
 @Yaml({
-  file: process.env.CORS_CONFIG_FILE_PATH || 'configs/cors.config.yml',
+  file:
+    process.env.CORS_CONFIG_FILE_PATH || 'configs/backend/dev.cors.config.yml',
   encoding: 'utf-8',
 })
 export class CorsConfig implements CorsOptions {
